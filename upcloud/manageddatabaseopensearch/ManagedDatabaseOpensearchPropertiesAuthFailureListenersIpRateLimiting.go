81f7f33971ca70892fe1441fea644bff55fd479f
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2024-07-19 03:28:33 +0000

    chore(release): v14.8.1

--- upcloud/manageddatabaseopensearch/ManagedDatabaseOpensearchPropertiesAuthFailureListenersIpRateLimiting.go
+++ upcloud/manageddatabaseopensearch/ManagedDatabaseOpensearchPropertiesAuthFailureListenersIpRateLimiting.go
@@ -7,27 +7,27 @@ package manageddatabaseopensearch
 type ManagedDatabaseOpensearchPropertiesAuthFailureListenersIpRateLimiting struct {
 	// The number of login attempts allowed before login is blocked.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_opensearch#allowed_tries ManagedDatabaseOpensearch#allowed_tries}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_opensearch#allowed_tries ManagedDatabaseOpensearch#allowed_tries}
 	AllowedTries *float64 `field:"optional" json:"allowedTries" yaml:"allowedTries"`
 	// The duration of time that login remains blocked after a failed login.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_opensearch#block_expiry_seconds ManagedDatabaseOpensearch#block_expiry_seconds}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_opensearch#block_expiry_seconds ManagedDatabaseOpensearch#block_expiry_seconds}
 	BlockExpirySeconds *float64 `field:"optional" json:"blockExpirySeconds" yaml:"blockExpirySeconds"`
 	// The maximum number of blocked IP addresses.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_opensearch#max_blocked_clients ManagedDatabaseOpensearch#max_blocked_clients}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_opensearch#max_blocked_clients ManagedDatabaseOpensearch#max_blocked_clients}
 	MaxBlockedClients *float64 `field:"optional" json:"maxBlockedClients" yaml:"maxBlockedClients"`
 	// The maximum number of tracked IP addresses that have failed login.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_opensearch#max_tracked_clients ManagedDatabaseOpensearch#max_tracked_clients}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_opensearch#max_tracked_clients ManagedDatabaseOpensearch#max_tracked_clients}
 	MaxTrackedClients *float64 `field:"optional" json:"maxTrackedClients" yaml:"maxTrackedClients"`
 	// The window of time in which the value for `allowed_tries` is enforced.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_opensearch#time_window_seconds ManagedDatabaseOpensearch#time_window_seconds}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_opensearch#time_window_seconds ManagedDatabaseOpensearch#time_window_seconds}
 	TimeWindowSeconds *float64 `field:"optional" json:"timeWindowSeconds" yaml:"timeWindowSeconds"`
 	// The type of rate limiting.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_opensearch#type ManagedDatabaseOpensearch#type}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_opensearch#type ManagedDatabaseOpensearch#type}
 	Type *string `field:"optional" json:"type" yaml:"type"`
 }
 
--- upcloud/manageddatabaseredis/ManagedDatabaseRedisConfig.go
+++ upcloud/manageddatabaseredis/ManagedDatabaseRedisConfig.go
@@ -26,46 +26,46 @@ type ManagedDatabaseRedisConfig struct {
 	//
 	// The name is used as a prefix for the logical hostname. Must be unique within an account
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_redis#name ManagedDatabaseRedis#name}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_redis#name ManagedDatabaseRedis#name}
 	Name *string `field:"required" json:"name" yaml:"name"`
 	// Service plan to use.
 	//
 	// This determines how much resources the instance will have. You can list available plans with `upctl database plans <type>`.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_redis#plan ManagedDatabaseRedis#plan}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_redis#plan ManagedDatabaseRedis#plan}
 	Plan *string `field:"required" json:"plan" yaml:"plan"`
 	// Title of a managed database instance.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_redis#title ManagedDatabaseRedis#title}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_redis#title ManagedDatabaseRedis#title}
 	Title *string `field:"required" json:"title" yaml:"title"`
 	// Zone where the instance resides, e.g. `de-fra1`. You can list available zones with `upctl zone list`.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_redis#zone ManagedDatabaseRedis#zone}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_redis#zone ManagedDatabaseRedis#zone}
 	Zone *string `field:"required" json:"zone" yaml:"zone"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_redis#id ManagedDatabaseRedis#id}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_redis#id ManagedDatabaseRedis#id}.
 	//
 	// Please be aware that the id field is automatically added to all resources in Terraform providers using a Terraform provider SDK version below 2.
 	// If you experience problems setting this value it might not be settable. Please take a look at the provider documentation to ensure it should be settable.
 	Id *string `field:"optional" json:"id" yaml:"id"`
 	// Maintenance window day of week. Lower case weekday name (monday, tuesday, ...).
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_redis#maintenance_window_dow ManagedDatabaseRedis#maintenance_window_dow}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_redis#maintenance_window_dow ManagedDatabaseRedis#maintenance_window_dow}
 	MaintenanceWindowDow *string `field:"optional" json:"maintenanceWindowDow" yaml:"maintenanceWindowDow"`
 	// Maintenance window UTC time in hh:mm:ss format.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_redis#maintenance_window_time ManagedDatabaseRedis#maintenance_window_time}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_redis#maintenance_window_time ManagedDatabaseRedis#maintenance_window_time}
 	MaintenanceWindowTime *string `field:"optional" json:"maintenanceWindowTime" yaml:"maintenanceWindowTime"`
 	// network block.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_redis#network ManagedDatabaseRedis#network}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_redis#network ManagedDatabaseRedis#network}
 	Network interface{} `field:"optional" json:"network" yaml:"network"`
 	// The administrative power state of the service.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_redis#powered ManagedDatabaseRedis#powered}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_redis#powered ManagedDatabaseRedis#powered}
 	Powered interface{} `field:"optional" json:"powered" yaml:"powered"`
 	// properties block.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.0/docs/resources/managed_database_redis#properties ManagedDatabaseRedis#properties}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.8.1/docs/resources/managed_database_redis#properties ManagedDatabaseRedis#properties}
 	Properties *ManagedDatabaseRedisProperties `field:"optional" json:"properties" yaml:"properties"`
 }
 
