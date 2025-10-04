4dcc1e3ffd2f983e0376da0d3522d63b3a1a55fc
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2025-10-04 03:30:43 +0000

    chore(release): v15.5.0

--- upcloud/dataupcloudhosts/DataUpcloudHostsHosts.go
+++ upcloud/dataupcloudhosts/DataUpcloudHostsHosts.go
@@ -7,7 +7,7 @@ package dataupcloudhosts
 type DataUpcloudHostsHosts struct {
 	// statistics block.
 	//
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.27.0/docs/data-sources/hosts#statistics DataUpcloudHosts#statistics}
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/upcloudltd/upcloud/5.28.0/docs/data-sources/hosts#statistics DataUpcloudHosts#statistics}
 	Statistics interface{} `field:"optional" json:"statistics" yaml:"statistics"`
 }
 
